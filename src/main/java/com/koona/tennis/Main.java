ea5fb35bee696997ea0b5419f38f501e85131133
Steve KOUNA <steve.kouna@afrinic.net>
2021-10-19 11:01:27 +0400

    exercice 5
    exercice 5

--- src/main/java/com/koona/tennis/Main.java
+++ src/main/java/com/koona/tennis/Main.java
@@ -15,13 +15,13 @@ import com.koona.tennis.controller.TournoiController;
  */
 public class Main {
     public static void main(String[] args) {
-        JoueurController controller = new JoueurController();
+//        JoueurController controller = new JoueurController();
 //        controller.afficheDetailsJoueur();
-        controller.creerJoueur();
+//        controller.creerJoueur();
         
-//        TournoiController tournoiCtl = new TournoiController();
+        TournoiController tournoiCtl = new TournoiController();
 //        tournoiCtl.afficheDetailsTournoi();
-//        tournoiCtl.creerTournoi();
+        tournoiCtl.creerTournoi();
     }
 }
 
